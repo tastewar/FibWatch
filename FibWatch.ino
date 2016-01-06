17f03a1003a7137c2c4f6628d90387ee3829c085
Tom Stewart <gitstuff@tastewar.com>
2016-01-05 21:43:17 -0500

    Initial commit of source files.

---
+++ FibWatch.ino
@@ -0,0 +1,286 @@
+#include <TinyScreen.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Time.h>
+#include <Timezone.h>
+
+// Colors
+#define	BLACK           0x00
+#define	BLUE            0xE0
+#define	RED             0x03
+#define	GREEN           0x1C
+#define	DGREEN          0x0C
+#define YELLOW          0x1F
+#define WHITE           0xFF
+#define ALPHA           0xFE
+#define	BROWN           0x32
+
+#define HOURCOLOR       RED
+#define MINUTECOLOR     GREEN
+#define BOTHCOLOR       BLUE
+#define OFFCOLOR        WHITE
+
+// Buttons
+#define BTN_TR          0b0001
+#define BTN_BR          0b0010
+#define BTN_BL          0b0100
+#define BTN_TL          0b1000
+
+// Flipped
+#define BTN_F_BL        0b0001
+#define BTN_F_TL        0b0010
+#define BTN_F_TR        0b0100
+#define BTN_F_BR        0b1000
+
+// Screen Dimensions
+#define WIDTH           96
+#define HEIGHT          64
+#define FIBSH			5
+#define FIBSW			8
+#define NBYHEIGHT       (HEIGHT/FIBSH)
+#define NBYWIDTH        (WIDTH/FIBSW)
+
+// Comment the below define if screen is right side up
+#define REVERSE
+// Comment the below define to remove borders
+#define BORDERS
+
+#if NBYHEIGHT<NBYWIDTH
+#define PixPerFib      NBYHEIGHT
+#else
+#define PixPerFib      NBYWIDTH
+#endif
+
+#define TopMargin (HEIGHT%(PixPerFib*FIBSH)>>1)
+#define LeftMargin (WIDTH%(PixPerFib*FIBSW)>>1)
+
+// Box Locations
+#ifndef REVERSE
+
+#define BOX1ATOP (TopMargin+PixPerFib)
+#define BOX1ALEFT (LeftMargin+(2*PixPerFib))
+#define BOX1BTOP TopMargin
+#define BOX1BLEFT (LeftMargin+(2*PixPerFib))
+#define BOX2TOP TopMargin
+#define BOX2LEFT LeftMargin
+#define BOX3TOP (TopMargin+(2*PixPerFib))
+#define BOX3LEFT LeftMargin
+#define BOX5TOP TopMargin
+#define BOX5LEFT (LeftMargin+(3*PixPerFib))
+
+#else
+
+#define BOX1ATOP (TopMargin+(3*PixPerFib))
+#define BOX1ALEFT (LeftMargin+(5*PixPerFib))
+#define BOX1BTOP (TopMargin+(4*PixPerFib))
+#define BOX1BLEFT (LeftMargin+(5*PixPerFib))
+#define BOX2TOP (TopMargin+(3*PixPerFib))
+#define BOX2LEFT (LeftMargin+(6*PixPerFib))
+#define BOX3TOP TopMargin
+#define BOX3LEFT (LeftMargin+(5*PixPerFib))
+#define BOX5TOP TopMargin
+#define BOX5LEFT LeftMargin
+
+#endif
+
+#define BOX1SIZE PixPerFib
+#define BOX2SIZE (2*PixPerFib)
+#define BOX3SIZE (3*PixPerFib)
+#define BOX5SIZE (5*PixPerFib)
+#define BOX1ABIT 0b00001
+#define BOX1BBIT 0b00010
+#define BOX2BIT  0b00100
+#define BOX3BIT  0b01000
+#define BOX5BIT  0b10000
+
+#define numcolors 4
+#define numboxes 5
+#define DISPLAYTIME 10000
+
+uint8_t clockcolors[]={OFFCOLOR,HOURCOLOR,MINUTECOLOR,BOTHCOLOR};
+uint8_t bits[]={BOX1ABIT,BOX1BBIT,BOX2BIT,BOX3BIT,BOX5BIT};
+uint8_t shifts[]={0,1,2,3,4};
+uint8_t lefts[numboxes]={BOX1ALEFT,BOX1BLEFT,BOX2LEFT,BOX3LEFT,BOX5LEFT};
+uint8_t tops[numboxes]={BOX1ATOP,BOX1BTOP,BOX2TOP,BOX3TOP,BOX5TOP};
+uint8_t sizes[numboxes]={BOX1SIZE,BOX1SIZE,BOX2SIZE,BOX3SIZE,BOX5SIZE};
+
+// ways to display numbers
+// arrays of bitmasks where the bits represent the state of boxes 5,3,2,1B,1A
+uint8_t   zeros[]={0b00000};
+uint8_t    ones[]={0b00010,0b00001};
+uint8_t    twos[]={0b00100,0b00011};
+uint8_t  threes[]={0b01000,0b00110,0b00101};
+uint8_t   fours[]={0b01010,0b01001,0b00111};
+uint8_t   fives[]={0b10000,0b01100,0b01011};
+uint8_t   sixes[]={0b10010,0b10001,0b01110,0b01101};
+uint8_t  sevens[]={0b10100,0b10011,0b01111};
+uint8_t  eights[]={0b11000,0b10110,0b10101};
+uint8_t   nines[]={0b11010,0b11001,0b10111};
+uint8_t    tens[]={0b11100,0b11011};
+uint8_t elevens[]={0b11110,0b11101};
+uint8_t twelves[]={0b11111};
+
+uint8_t *waystodraw[]={zeros,ones,twos,threes,fours,fives,sixes,sevens,eights,nines,tens,elevens,twelves};
+uint8_t numwaystodraw[]={
+	sizeof(zeros)/sizeof(uint8_t),
+	sizeof(ones)/sizeof(uint8_t),
+	sizeof(twos)/sizeof(uint8_t),
+	sizeof(threes)/sizeof(uint8_t),
+	sizeof(fours)/sizeof(uint8_t),
+	sizeof(fives)/sizeof(uint8_t),
+	sizeof(sixes)/sizeof(uint8_t),
+	sizeof(sevens)/sizeof(uint8_t),
+	sizeof(eights)/sizeof(uint8_t),
+	sizeof(nines)/sizeof(uint8_t),
+	sizeof(tens)/sizeof(uint8_t),
+	sizeof(elevens)/sizeof(uint8_t),
+	sizeof(twelves)/sizeof(uint8_t),
+};
+
+//US Eastern Time Zone (New York, Detroit)
+TimeChangeRule usEDT = {"EDT", Second, Sun, Mar, 2, -240};  //Eastern Daylight Time = UTC - 4 hours
+TimeChangeRule usEST = {"EST", First, Sun, Nov, 2, -300};   //Eastern Standard Time = UTC - 5 hours
+Timezone usET(usEDT, usEST);
+
+uint8_t ChooseRepresentation ( uint8_t number )
+{
+  uint8_t possibilities, whichway, *ways;
+  if ( number>12 ) return 0;
+  possibilities=numwaystodraw[number];
+  ways=waystodraw[number];
+  if ( possibilities>1 )
+  {
+    whichway=random(possibilities);
+  }
+  else
+  {
+  	whichway=0;
+  }
+  return ways[whichway];
+}
+
+TinyScreen display = TinyScreen(0);
+
+void FillFibBox(uint8_t box, uint8_t color)
+{
+  if (box<numboxes)
+  {
+#ifdef BORDERS
+   	display.drawRect(lefts[box]+1,tops[box]+1,sizes[box]-2,sizes[box]-2,true,color);
+#else
+   	display.drawRect(lefts[box],tops[box],sizes[box],sizes[box],true,color);
+#endif
+  }
+}
+
+//Function to return the compile date and time as a time_t value
+time_t compileTime(void)
+{
+#define FUDGE 25        //fudge factor to allow for compile time (seconds, YMMV)
+
+    char *compDate = __DATE__, *compTime = __TIME__, *months = "JanFebMarAprMayJunJulAugSepOctNovDec";
+    char chMon[3], *m;
+    int d, y;
+    tmElements_t tm;
+    time_t t;
+
+    strncpy(chMon, compDate, 3);
+    chMon[3] = '\0';
+    m = strstr(months, chMon);
+    tm.Month = ((m - months) / 3 + 1);
+
+    tm.Day = atoi(compDate + 4);
+    tm.Year = atoi(compDate + 7) - 1970;
+    tm.Hour = atoi(compTime);
+    tm.Minute = atoi(compTime + 3);
+    tm.Second = atoi(compTime + 6);
+    t = makeTime(tm);
+    return t + FUDGE - (5*3600);        //add fudge factor to allow for compile time and TZO
+}
+
+void DisplayTime (time_t loc)
+{
+  uint8_t hrsr,minsr,b;
+
+  hrsr=ChooseRepresentation(hour(loc)%12);
+  minsr=ChooseRepresentation(minute(loc)/5);
+
+#ifdef DEBUG
+  uint8_t prhrs,prmins;
+
+  prhrs=(((hrsr&BOX5BIT)>>4)*5)+(((hrsr&BOX3BIT)>>3)*3)+(((hrsr&BOX2BIT)>>2)*2)+((hrsr&BOX1BBIT)>>1)+(hrsr&BOX1ABIT);
+  prmins=(((minsr&BOX5BIT)>>4)*5)+(((minsr&BOX3BIT)>>3)*3)+(((minsr&BOX2BIT)>>2)*2)+((minsr&BOX1BBIT)>>1)+(minsr&BOX1ABIT);
+  Serial.print("Time: ");
+  Serial.print(hour(loc));
+  Serial.print(':');
+  Serial.println(minute(loc));
+  Serial.print("Rep: ");
+  Serial.print(hrsr,BIN);
+  Serial.print(':');
+  Serial.println(minsr,BIN);
+  Serial.print("Recalculated Clock Hands: ");
+  Serial.print(prhrs);
+  Serial.print(':');
+  Serial.println(prmins);
+#endif
+  for (b=0;b<numboxes;b++)
+  {
+  	uint8_t col,colnum,XXX,YYY;
+  	XXX=(hrsr&bits[b])>>shifts[b];
+  	YYY=((minsr&bits[b])>>shifts[b])<<1;
+  	colnum=XXX+YYY;
+#ifdef DEBUG
+    Serial.print("Box Color for box ");
+  	Serial.print(b);
+  	Serial.print(':');
+  	Serial.print(XXX);
+  	Serial.print(':');
+  	Serial.println(YYY);
+  	Serial.println(colnum);
+#endif
+    col=clockcolors[colnum];
+  	FillFibBox(b,col);
+  }
+}
+
+// Globals
+time_t utc;
+bool DisplayOn;
+bool BtnPressNoted;
+unsigned long TimeDisplayOn;
+
+void setup()
+{
+#ifdef DEBUG
+  Serial.begin(115200);
+#endif
+  setTime(usET.toUTC(compileTime()));
+  Wire.begin();
+  display.begin();
+  display.clearWindow(0,0,WIDTH,HEIGHT);
+}
+
+void loop()
+{
+  time_t loc;
+#ifdef DEBUG
+  Serial.println();
+#endif
+  if ( display.getButtons() & BTN_TL )
+  {
+  	TimeDisplayOn = millis();
+  	if ( !DisplayOn )
+  	{
+      utc = now();
+      loc = usET.toLocal(utc);
+      DisplayTime(loc);
+      display.on();
+      DisplayOn=true;
+  	}
+  }
+  else if ( millis() - TimeDisplayOn > DISPLAYTIME )
+  {
+    display.off();
+    DisplayOn=false;
+  }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,27 @@
+This watch will run on the TinyCircuits TinyDuino platform with the TinyScreen
+and TinyShield RTC. The intent is to have it sleep almost all the time, awakening
+only when a button is pressed. Buttons are as follows:
+
+ |==========|
++1          2+
+ |          |
+ |          |
++3          4+
+ |==========|
+
+
+Button 1 when pressed will display the time until 5 seconds after released
+Button 2 enters time setting mode
+
+When in time setting mode, Button 1 exits, Button 3 retreats current item,
+Button 4 advances current item, and Button 2 accepts input.
+
+When first entering time setting mode, you can choose from a list of time zones.
+Once the TZ is accepted, the change goes into effect immediately for the date
+and time setting portion. We then go through Year, Month, Date, Hour, Minute,
+Second. If Button 1 is pressed at any time in the process, the current and any
+subsequent items are left as is, but any already accepted data takes effect. If
+no input is detected for 30 seconds, proceed as if Button 1 was pressed.
+
+If buttons 1 and 2 are pressed together, the time is displayed as text, and the
+display is turned off 5 seconds later.
